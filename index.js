1e5d8077192454f7847ed80aab1be1d146635a52
Detlolka <detlolka@yahoo.com>
2023-09-30 19:51:33 +0300

    create task H

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ import "./src/sprint_2_tasks/task_D";
 import "./src/sprint_2_tasks/task_E";
 import "./src/sprint_2_tasks/task_F";
 import "./src/sprint_2_tasks/task_G";
+import "./src/sprint_2_tasks/task_H";
+
 
 
